07b86cd21d7e24aecb159a5178abfe49009f0003
Subhra Roy <subhra.roy@e-arc.com>
2019-09-30 19:01:04 +0530

    done

--- AlgorithmDevDemo/AppDelegate.swift
+++ AlgorithmDevDemo/AppDelegate.swift
@@ -11,8 +11,6 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -23,6 +21,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+        if #available(iOS 13.0, *) {
+             let configuration = UISceneConfiguration(name: "MyAppConfiguration", sessionRole: connectingSceneSession.role)
+             configuration.sceneClass = UIWindowScene.classForCoder()
+             configuration.delegateClass = SceneDelegate.classForCoder()
+             return configuration
+        } else {
+            
+        }
+       
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
--- AlgorithmDevDemo/SceneDelegate.swift
+++ AlgorithmDevDemo/SceneDelegate.swift
@@ -18,6 +18,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let rootViewController : ViewController = ViewController(nibName: "ViewController", bundle: Bundle.main)
+        let navigationController : UINavigationController = UINavigationController(rootViewController: rootViewController)
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+               
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
